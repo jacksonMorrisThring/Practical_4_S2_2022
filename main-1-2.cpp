8bc8f0dc336514d5722a5a88ff1fb5d5529e0027
Jackson Morris-Thring <jacksonmorris-thring@student-10-201-32-229.wireless.adelaide.edu.au>
2022-08-31 12:58:24 +0930

    1-2 edit

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -2,9 +2,8 @@
 #include "Person.h"
 
 int main(){
-    Person c1;
+    Person c1(50000, "TJ");
 
-    std::cout << "Name: " << c1.getName() << "   Age: " << c1.getSalary() << std::endl;
     c1.setName("John");
     c1.setSalary(150000);
     std::cout << "Name: " << c1.getName() << "   Age: " << c1.getSalary() << std::endl;
